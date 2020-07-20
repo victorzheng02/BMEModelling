bacbb5e4f15508ce9318e0a1f9e55c170921b1ac
unknown <diveshb03@gmail.com>
2020-07-20 16:44:52 -0400

    My First Commit

---
+++ Assets/MoveTowards.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class MoveTowards : MonoBehaviour
+{
+    public Transform Coronavirus;
+    public float speed = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, Coronavirus.transform.position, speed * Time.deltaTime);
+    }
+}
---
+++ Assets/StandardMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class StandardMove : MonoBehaviour
+{
+
+    public float speed = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(0, speed * Time.deltaTime, 0);
+
+    }
+}
