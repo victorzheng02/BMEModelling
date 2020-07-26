1eb24a9095fd1e1f3c695d0bb1e27486eed46a51
smilesthesad <vzheng@gmail.com>
2020-07-26 01:21:20 -0400

    finished main settings GUI

---
+++ Assets/Scripts/RelativeHumidity.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class RelativeHumidity : MonoBehaviour{
+    Text valueText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        valueText = GetComponent<Text> ();
+    }
+
+    // Update is called once per frame  
+    public void textUpdate(float value)
+    {
+        valueText.text = Mathf.RoundToInt((value * 40) + 20).ToString() + "%";
+    }
+}
---
+++ Assets/Scripts/ShowValueScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ShowValueScript : MonoBehaviour{
+    Text valueText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        valueText = GetComponent<Text> ();
+    }
+
+    // Update is called once per frame  
+    public void textUpdate(float value)
+    {
+        int scale = Mathf.RoundToInt(value*100);
+        int temp = Mathf.RoundToInt(scale/9) + 24;
+        valueText.text = temp.ToString() + "°C";
+    }
+}
